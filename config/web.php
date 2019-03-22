31a952976f5ad69e35b605811d63aa75ce6f05a4
Alexande <l4legenda@gmail.com>
2019-03-22 20:21:42 +0500

    add htaccess and changed config/web

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dACzOuYIsXJxfDIFz5m1KQU57riW0ZCX',
+            'baseUrl' => ''
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,15 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                "" => "site/index"
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
